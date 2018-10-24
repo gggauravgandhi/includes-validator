5e326d3221d20d2f482ad14599a1b0748efac1a7
Gaurav <imgg.gauravgandhi@gmail.com>
2018-10-24 22:42:09 +0530

    Initial codebase + eslint
    Signed-off-by: Gaurav <imgg.gauravgandhi@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # includes-validator
 Provides validator for general purpose testing to identify existence of some pattern/object in text
+
+## Install
+
+### NPM
+```sh
+npm install includes-validator
+```
+
+
+## Usage
+
+```js
+const includes = require('includes-validator');
+
+const includesPhone = includes.phone(someString);
+const includesEmail = includes.email(someString);
+
+console.log(`Does string includes phone? ${includesPhone}`);
+console.log(`Does string includes email? ${includesEmail}`);
+```
---
+++ includes-validator.js
@@ -0,0 +1,45 @@
+const libphone = require('libphonenumber-js');
+
+const includes = {};
+
+includes.phoneRegxs = [/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/];
+
+includes.emailRegxs = [/[a-zA-Z0-9-_.]+@[a-zA-Z0-9-_.]+/];
+
+
+includes.number = (data, options) => {
+  const countries = options.countries || ['US', 'IN'];
+
+  // List of ISO country codes: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+
+  let exists = false;
+  includes.phoneRegxs.forEach((regx) => {
+    exists = regx.test(data);
+  });
+  countries.forEach((countryCode) => {
+    const len = libphone.findNumbers(data, countryCode, { v2: true }).length;
+    if (len > 0) {
+      exists = true;
+    }
+  });
+  return exists;
+};
+
+
+includes.email = (data) => {
+  let exists = false;
+  includes.emailRegxs.forEach((regx) => {
+    exists = regx.test(data);
+  });
+  return exists;
+};
+
+
+// Export the Includes object for **Node.js**, with
+// backwards-compatibility for their old module API.
+if (typeof module !== 'undefined' && module.exports) {
+  exports = includes;
+  module.exports = includes;
+} else {
+  exports.includes = includes;
+}
